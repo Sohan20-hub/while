ba9f09987808499031f48597acc153fcbb61da07
sohan <102313089+Sohan20-hub@users.noreply.github.com>
2023-11-09 10:46:34 +0530

    Update file3.sh

--- file3.sh
+++ file3.sh
@@ -1,1 +1,15 @@
-
+#!\bin\bash
+read n
+n1=0
+n2=1
+count=2
+echo "Fibonacci series:"
+echo $n1
+echo $n2
+while [ $count -lt $n ]
+do
+fib=`expr $n1 + $n2`
+n1=$n2
+n2=$fib
+count=`expr $count + 1`
+done
